cdff4fd6071136dc02ee74c6d3f43dd45d3567ae
Iryna <kytsenkoiryna33@gmail.com>
2025-11-03 00:15:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Discount
+{
+    public Func<double, double> strategy;
+
+    public void SetStrategy(Func<double, double> s) => strategy = s;
+    public void Apply(double price)
+    {
+        Console.WriteLine("Ціна після знижки: " + strategy(price) + " грн");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var d = new Discount();
+
+        d.SetStrategy(p => p * 0.9); // 10% знижка
+        d.Apply(100);
+
+        d.SetStrategy(p => p * 0.8); // 20% знижка
+        d.Apply(100);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
